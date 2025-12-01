b23a0145938c91dd2aae2213b8b5fad5f3263f20
pragati0209 <pragatiaggimath09@gmail.com>
2025-12-01 16:40:49 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -8,8 +8,8 @@ def employee_details(name, emp_id, department, salary):
     return result
 
 if __name__ == "__main__":
-    name = "Alice"
-    emp_id = "E1001"
-    department = "IT"
-    salary = 55000
-    print(employee_details(name, emp_id, department, salary))
+    name ="Alice"
+    emp_id ="E1001"
+    department ="IT"
+    salary =55000
+    print(employee_details(name, emp_id,department, salary))
